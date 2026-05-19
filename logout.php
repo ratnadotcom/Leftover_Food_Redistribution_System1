519eac89a9eca40d21749edd6bd470a21dd34e6d
Mst.Ratna <158990935+ratnadotcom@users.noreply.github.com>
2026-05-19 19:57:11 +0600

    Simplify logout process and redirect

--- logout.php
+++ logout.php
@@ -1,12 +1,6 @@
 <?php
-require_once 'session_check.php';
-if(is_logged_in()){
-    session_unset();
-    if(ini_get("session.use_cookies")){
-        $p=session_get_cookie_params();
-        setcookie(session_name(),'',time()-42000,$p['path'],$p['domain'],$p['secure'],$p['httponly']);
-    }
-    session_destroy();
-}
-header("Location:login.php?logged_out=1"); exit();
+// logout.php
+require_once 'includes/config.php';
+session_destroy();
+redirect('/food_system/login.php');
 ?>
